ec310d550990fa902bba913796479509c2979bb9
Lara <larat248@gmail.com>
2020-01-26 03:05:53 -0500

    token

--- conuhacks-backend/server.js
+++ conuhacks-backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cookie = require("cookie-parser");
+const cors = require("cors");
+var request = require('request'); // "Request" library
+
 
 var app = express();
 
 const keys = require("./keys");
 const sessionModel = require("./models/session");
 const queueModel = require("./models/queue");
+var querystring = require('querystring');
+
+const clientId = keys.SPOTIFY_CLIENT_ID;
+const clientSecret = keys.SPOTIFY_CLIENT_SECRET;
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+
 
 const url = `mongodb+srv://${keys.MONGO_USER}:${keys.MONGO_PASSWORD}@conuhacks2020-njuuz.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(url, {
@@ -19,6 +29,8 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+app.use(cookie());
+app.use(cors());
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
@@ -38,23 +50,14 @@ app.post("/session/create", async (req, res) => {
     res.status(500).send(err);
   }
 });
-//create session
-app.post("/session/create", async (req, res) => {
-  const session = new sessionModel(req.body);
-  try {
-    await session.save();
-    res.send(session);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+
 
 //get sessions
-app.get("/session", async (req, res) => {
-  const sessions = await sessionModel.find({});
+app.get("/session/:sessionName", async (req, res) => {
+  const session = await sessionModel.find({sessionName: req.sessionName});
 
   try {
-    res.send(sessions);
+    res.send(session);
   } catch (err) {
     res.status(500).send(err);
   }
@@ -63,7 +66,7 @@ app.get("/session", async (req, res) => {
 //join session
 app.post("/session/join", async (req, res) => {
   const session = await sessionModel.findOneAndUpdate(
-    { name: req.body.sessionName },
+    { sessionName: req.body.sessionName },
     { $push: { guests: req.body.guestName } }
   );
   try {
@@ -140,3 +143,115 @@ app.delete("/queue", async(req,res)=> {
   }
   
 })
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+var stateKey = 'spotify_auth_state';
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('http://localhost:4200/room/partyhub?' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
